88a6d4ac54e9da3036781c447efb331595c82607
Naman Tuli <63744860+namantuli18@users.noreply.github.com>
2021-07-23 20:13:00 +0530

    Update setup_env.sh

--- src/setup_env.sh
+++ src/setup_env.sh
@@ -10,7 +10,8 @@ export PYTHONBIN="python"
 #export LD_LIBRARY_PATH=/anotherpath:LD_LIBRARY_PATH
 
 # Basic Theano flags
-MERLIN_THEANO_FLAGS="cuda.root=/usr/local/8.0,floatX=float32,on_unused_input=ignore"
+MERLIN_THEANO_FLAGS="device=cuda,floatX=float32,on_unused_input=ignore"
+
 export MERLIN_THEANO_FLAGS
 	
 
